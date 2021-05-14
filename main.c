25205d83b08e8771cd8d6ada9b8e4471e4f86e3f
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2021-05-14 19:10:55 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
@@ -28,17 +27,17 @@ int main()
         //traiter les cas des characteres  de navires :
         Navire a1,a2,a3;
         char a[6][6],rejouer,quitter;
-        int i,j,position,rate=0,win=0,ho;
+        int i,j,position,rate=0,win=0,ho,choixOrdinateur;
         int x,y,c=0,col=0,lig=0,re;
         int navirePrecedent,choix,navire;
         int success=1;
         int tentative=3;
         char p1[10];//r aire stocker le nom des deux joueurs:
         char p2[10];
-system("COLOR 6");
+    system("COLOR 6");
     Debut:
     Menu();
-    gotoxy(28,17);scanf("%d",&choix);
+    gotoxy(28,18);scanf("%d",&choix);
 do
 {
     switch(choix)
@@ -112,7 +111,7 @@ do
                                                    gotoxy(50,10);printf(" ");
                                                    gotoxy(50,12);printf(" ");
 
-                                                  } while(a2.y1>4);
+                                                  } while(a2.y1>4 || a2.x1>3);
                                                  if(a[a2.x1][a2.y1]=='-' && a[a2.x1+1][a2.y1]=='-' && a[a2.x1+2][a2.y1]=='-'){
                                                    navirePrecedent=0;
                                                   a[a2.x1][a2.y1]='B';
@@ -149,7 +148,7 @@ do
                                                    gotoxy(4*(a3.y1+1)+5,2*a3.x1+7);cprintf("%c",a[a3.x1][a3.y1]);
                                                    gotoxy(4*(a3.y1+2)+5,2*a3.x1+9);cprintf("%c",a[a3.x1+1][a3.y1+1]);
                                                    gotoxy(4*(a3.y1+3)+5,2*a3.x1+11);cprintf("%c",a[a3.x1+2][a3.y1+2]);
-                                           }
+                                                 }
                                                 }while(navirePrecedent==1);
                                             } break;
                         }
@@ -158,7 +157,7 @@ do
             system("cls");
             printf("Vous avez  Deployer vos trois navires Avec succes  ");
             joueur2();
-             SCORE=0;
+            SCORE=0;
             while(SCORE<=9 && tentative>=0)
             {
                 gotoxy(40,3);printf("il vous reste %d tentative ",tentative);
@@ -203,7 +202,7 @@ do
                             break;
                      }
             }
-        }
+        } break;
    case 2:
        {
            system("cls");
@@ -224,10 +223,129 @@ do
 
    case 3:
     {
+        system("cls");
         puts("\n************JOUER CONTRE ORDINATEUR************\n");
+        for(i=0;i<6;i++)
+        {
+            for(j=0;j<6;j++)
+            {
+                a[i][j]='-'; //initialisation de la matrice :
+            }
+        }
+       srand(time(NULL));
 
+       while(success<=3)
+       {
+           choixOrdinateur=rand()%4+1;
+           switch (choixOrdinateur)
+           {
+                            case 1:
+                                 {
+                                       do
+                                           {
+                                               navirePrecedent=1;
+                                                  do{
+                                                    a1.x1=rand()%6;
+                                                    }while(a1.x1<0 || a1.x1>5);
+                                                 if(a[a1.x1][a1.y1]=='-' && a[a1.x1][a1.y1+1]=='-' && a[a1.x1][a1.y1+2]=='-'){
+                                                    navirePrecedent=0;//la valeur va changer lors du deploiement d'un certain navire
+                                                    a[a1.x1][a1.y1]='A';
+                                                    a[a1.x1][a1.y1+1]='A';
+                                                    a[a1.x1][a1.y1+2]='A';
+                                                    success++;
+                                                         }
+                                             } while(navirePrecedent==1);
+                                 } break;
+                            case 2:
+                                {
+                                    do
+                                        {
+                                                  navirePrecedent=1;
+                                              do
+                                                   {
+                                                       a2.y1=rand()%6;
+                                                       a2.x1=rand()%4;
+                                                   } while(a2.y1>4 || a2.x1>3);
+                                                 if(a[a2.x1][a2.y1]=='-' && a[a2.x1+1][a2.y1]=='-' && a[a2.x1+2][a2.y1]=='-'){
+                                                   navirePrecedent=0;
+                                                  a[a2.x1][a2.y1]='B';
+                                                  a[a2.x1+1][a2.y1]='B';
+                                                  a[a2.x1+2][a2.y1]='B';
+                                                  success++;
+                                               }
+                                        } while(navirePrecedent==1);
+                                }break;
+                            case 3:
+                                {
+                                    do
+                                          {
+                                                    navirePrecedent=1;
+                                                  do
+                                                    {
+                                                        a3.y1=rand()%5;
+                                                        a3.x1=rand()%5;
+                                                    } while(a3.y1>=4 || a3.x1>=4);
+                                                 if(a[a3.x1][a3.y1]=='-' && a[a3.x1+1][a3.y1+1]=='-' && a[a3.x1+2][a3.y1+2]=='-'){
+                                                   navirePrecedent=0;
+                                                   success++;
+                                                   a[a3.x1][a3.y1]='C';
+                                                   a[a3.x1+1][a3.y1+1]='C';
+                                                   a[a3.x1+2][a3.y1+2]='C';
+                                                 }
+                                           }while(navirePrecedent==1);
+                                } break;
 
+       }
     }
+
+    //le tour pour  l'utilisateur :
+            joueur2();
+            SCORE=0;
+            while(SCORE<=9 && tentative>=0)
+            {
+                gotoxy(40,3);printf("il vous reste %d tentative ",tentative);
+                //voir si le joueur a bien saisi le debut de cchaque navire sinon il ya une diminution du score;
+                gotoxy(50,10);printf("donner x : ");
+                gotoxy(50,11);scanf("%d",&x);
+                gotoxy(50,13);printf("donnee y : ");
+                gotoxy(50,14);scanf("%d",&y);
+                gotoxy(50,11);printf(" ");
+                gotoxy(50,14);printf(" ");
+                if(a[x][y]=='A' || a[x][y]=='B' || a[x][y]=='C' )
+                {
+                    if(a1.x1==x && a1.y1==y)
+                    {
+                        gotoxy(4*(y+1)+5,2*x+7);printf("O");
+                        gotoxy(50,20);printf("Vous avez touche le navire A");
+                        SCORE+=3;
+                    } else if(x==a2.x1 && a2.y1==y)
+                    {
+                       gotoxy(4*(y+1)+5,2*x+7);printf("X");
+                       gotoxy(50,20);printf("Vous avez touche le navire B");
+                       SCORE+=3;
+                    } else if(a3.x1==x && a3.y1==y)
+                    {
+                      gotoxy(4*(y+1)+5,2*x+7);printf("C");
+                      gotoxy(50,20);printf("Vous avez touche le navire C");
+                      SCORE+=3;
+                    }
+                } else
+                {
+                  SCORE-=3;
+                     if(SCORE<0)
+                    {
+                       SCORE=0;
+                    }
+                }
+                gotoxy(40,6);printf("Votre score actuel est : %d",SCORE);
+                tentative--;
+                if(SCORE==9)
+                     {
+                            printf("\nVous avez mis tous les navires a terres   ! !  ! \n");
+                            break;
+                     }
+            }
+  } break;
     case 4:
     {
         puts("Voulez-vous vraiment quitter le jeu O/N?");
@@ -250,6 +368,15 @@ do
             } while(quitter!='o' && quitter!='O' && quitter!='N' && quitter!='n');
         }
     } break;
+    default:
+        {
+            do
+            {
+              printf("choix erronee veuillez resaisir ");
+              scanf("%d",&choix);
+            } while(choix>3);
+
+        }
    }
     if(success==3)
     {
@@ -275,9 +402,10 @@ void Menu()
        gotoxy(27,11);printf(" ³       VEUILLEZ CONFIRMER VOTRE CHOIX PAR ENTREE          ³\n");
        gotoxy(27,12);printf(" ³           1-DEMARRER UNE NOUVELLE PARTIE                 ³ \n");
        gotoxy(27,13);printf(" ³           2-PARAMETRE ET REGLE DU JEU                    ³ \n");
-       gotoxy(27,14);printf(" ³                3-QUITTER LE JEU                          ³\n");
-       gotoxy(28,15);printf("ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ");
-       gotoxy(29,16);puts("Selectionner votre choix ");
+       gotoxy(27,14);printf(" ³           3-Jouer avec Ordinateur                        ³\n");
+       gotoxy(27,15);printf(" ³           4-QUITTER LE JEU                               ³\n");
+       gotoxy(28,16);printf("ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ");
+       gotoxy(29,17);puts("Selectionner votre choix ");
  }
     void joueur1(){
     system("COLOR 6");
@@ -316,10 +444,9 @@ void Menu()
     }
 
     void regleGeneral(){
-
-            printf("\t\t\t\tRegle générale du jeu \n") ;
-            printf ("RÈGLES DU JEU:\n") ;
-	    printf ("1) Il s'agit d'un jeu à deux joueurs, mais chaque joueur utilise son propre ordinateur et sa propre instance de ce programme ;
+        printf("\t\t\t\tRegle générale du jeu \n") ;
+        printf ("RÈGLES DU JEU:\n") ;
+	    printf ("1) Il s'agit d'un jeu à deux joueurs, mais chaque joueur utilise son propre ordinateur et sa propre instance de ce programme") ;
 	    printf ("2. le joueur sera invité à choisir la formation initiale de ses navires\n") ;
 	    printf (" pour le plateau de jeu \n") ;
 	    printf ("3. Il y a 6 types de formations à placer \n") ;
@@ -327,12 +454,10 @@ void Menu()
 	    printf ("5. l'ordinateur sélectionne aléatoirement l'endroit où le prochain tir du joueur sera effectué (coordonnées en GREC) \n") ;
 	    printf ("6. Lorsque l'ennemi frappe le joueur, l'utilisateur doit saisir les coordonnées du tir entrant (en lettres grecques)\n") ;
 	    printf ("7. Le jeu commence alors que chaque joueur tente de deviner l'emplacement des navires\n") ;
-// 	    printf (" du plateau de jeu de l'adversaire ; [*] touché et [X] manqué\n") ;
-	    printf ("8. Le premier joueur qui a deviné l'emplacement de tous les navires a gagné...") ;
-
-Traduit avec www.DeepL.com/Translator (version gratuite)
+	    printf (" du plateau de jeu de l'adversaire ; [*] touché et [X] manqué\n") ;
+	    printf ("8. Le premier joueur qui a deviné l'emplacement de tous les navires a gagné...\n") ;
     }
-int Ordinateur(char G[6][6])
+int Getcoordinates(char G[6][6])
 {
     int x,y;
     srand(time(NULL));
